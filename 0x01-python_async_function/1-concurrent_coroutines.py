d37b547a9c2f2bd0b8f2c1d059335040277dc7cf
St Pardon <okc4pardon@gmail.com>
2023-02-06 17:59:48 +0100

    :wrench: used the asyncio.gather() approach

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,12 +4,14 @@
 import asyncio
 from typing import List
 
+
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List:
     '''return a list of awaited response from previous function
     '''
-    res: List = []
-    for _ in range(n):
-        res.append(await wait_random(max_delay))
+    res: List  = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
     return sorted(res)
