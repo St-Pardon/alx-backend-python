c89ec2a193e41ccbc72923b2db918623a7321672
St Pardon <okc4pardon@gmail.com>
2023-02-06 18:04:00 +0100

    :wrench: fixed type annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,10 +8,10 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''return a list of awaited response from previous function
     '''
-    res: List  = await asyncio.gather(
+    res = await asyncio.gather(
         *tuple(map(lambda _: wait_random(max_delay), range(n)))
     )
     return sorted(res)
