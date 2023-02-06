d163a75c1ecc118286a937a6e388066f4dc5133f
St Pardon <okc4pardon@gmail.com>
2023-02-06 18:52:28 +0100

    :wrench: fixed type annotation brackets

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ import asyncio
 time_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List(float):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''return a list of awaited response from previous function
     '''
     res = await asyncio.gather(
