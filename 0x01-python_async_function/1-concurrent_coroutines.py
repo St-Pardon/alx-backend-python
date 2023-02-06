d745d21193dad9e139379c10ecbde799216bd083
St Pardon <okc4pardon@gmail.com>
2023-02-06 17:51:42 +0100

    :wrench: another test attempt

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,9 +4,7 @@
 import asyncio
 from typing import List
 
-
 wait_random = __import__('0-basic_async_syntax').wait_random
-
 
 async def wait_n(n: int, max_delay: int) -> List:
     '''return a list of awaited response from previous function
