f64bed3f7945c12c36f75cf9c756b77e011dc0df
St Pardon <okc4pardon@gmail.com>
2023-02-07 17:14:25 +0100

    :white-check-mark: completed task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Asynchronous Python
+'''
+import random
+import asyncio
+
+
+async def async_generator():
+    '''yeild a random number at 1sec interval 
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
